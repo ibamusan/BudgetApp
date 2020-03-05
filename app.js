5e701c8ecf8404f3df5b0466bb850f89794a2cc5
Ibrahim Amusan <ibamusan@gmail.com>
2020-03-05 08:30:43 +0100

    Initialization function added

--- app.js
+++ app.js
@@ -41,7 +41,20 @@ var UIController = (function(){
 // GENERAL APP CONTROLLER 
 var controller = (function(budgetCtrl, UICtrl){
 
-  var DOM = UICtrl.getDOMStrings();
+  var setUpEventListener = function(){
+
+    var DOM = UICtrl.getDOMStrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click' , ctrlAddItem)
+
+    document.addEventListener('keypress', function(event){
+
+    if(event.keyCode === 13 || event.which === 13){
+      ctrlAddItem();
+    }
+
+  });
+
+  };
 
   var ctrlAddItem = function (){
     // 1. Get the field input data
@@ -51,17 +64,16 @@ var controller = (function(budgetCtrl, UICtrl){
     // 4. Calculate the budget
     // 5. Display the budget on the UI
     console.log(input);
+  };
+
+  return {
+    init : function(){
+      setUpEventListener();
+      console.log('The Application has started.');
+    }
   }
 
-
-  document.querySelector(DOM.inputBtn).addEventListener('click' , ctrlAddItem)
-
-  document.addEventListener('keypress', function(event){
-
-    if(event.keyCode === 13 || event.which === 13){
-      ctrlAddItem();
-    }
-
-  })
-
 })(budgetController, UIController);
+
+
+controller.init();
